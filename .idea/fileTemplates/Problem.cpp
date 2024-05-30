d9dfb5511bb1a77a1b3cf18a65bbf5b8e4fa1ed7
YamanSD <mysd.real@gmail.com>
2024-05-30 15:24:49 +0300

    Solved

---
+++ .idea/fileTemplates/Problem.cpp
@@ -0,0 +1,21 @@
+/**
+ * ${NAME}
+ * ${TIME} ${DATE}
+ */
+#[[#include]]# <bits/stdc++.h>
+#[[#include]]# <ext/pb_ds/assoc_container.hpp>
+#[[#include]]# <ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+#[[#define]]# int long long
+#[[#define]]# unsigned unsigned int
+#[[#define]]# double long double
+#[[#define]]# fast ios_base::sync_with_stdio(false);cin.tie(NULL);
+#[[#define]]# nl '\n'
+template<typename T>
+using ordered_set = tree<T, null_type, less<>, rb_tree_tag, tree_order_statistics_node_update>;
+
+void solve() {
+    fast
+}
